6aee7da978b6b6b18af1a40720a1dfb9f82728ab
Pranab Amatya <pranab.aamtya.pa@gmail.com>
2021-01-15 00:15:03 +1100

    WIp

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,13 +24,7 @@ const  Nav = () => {
                     <li><Link href='/Contact'>Contact</Link></li>
                     <li><Link href='/Gallary'>Gallary</Link></li>
                 </ul>
-                {/* <select onchange="if (this.value) window.location.href = this.value;" className="responsiv-nav">
-                    <option value="/Home">Home<a href='/Home'>Home</a></option>
-                    <option value="/About">About</option>
-                    <option value="/Services">Services</option>
-                    <option value="#">Contact</option>
-                    <option value="#">Gallary</option>
-                </select> */}
+                
             </nav>
         </div>
     )
